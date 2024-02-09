236e4fa37d1656407572de21e432205082c50408
Samarth Shrivastava <shrivastavasamarth22@gmail.com>
2024-02-09 19:52:51 +0530

    added live cursor functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
+"use client";
+
+import Live from "@/components/Live";
+
 export default function Page() {
-	return <h1 className="text-5xl text-white">Hello World</h1>;
+	return (
+		<div>
+			<Live />
+		</div>
+	);
 }
