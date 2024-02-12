2d142b836d595766c814aa21168c054113008928
Samarth Shrivastava <shrivastavasamarth22@gmail.com>
2024-02-12 11:09:50 +0530

    added left and right sidebar and integrated canvas

--- components/Live.tsx
+++ components/Live.tsx
@@ -12,7 +12,11 @@ import ReactionSelector from "./reaction/ReactionButton";
 import FlyingReaction from "./reaction/FlyingReaction";
 import useInterval from "@/hooks/useInterval";
 
-const Live = () => {
+type Props = {
+	canvasRef: React.MutableRefObject<HTMLCanvasElement | null>;
+};
+
+const Live = ({ canvasRef }: Props) => {
 	const others = useOthers();
 	const [{ cursor }, updateMyPresence] = useMyPresence() as any;
 
@@ -54,7 +58,7 @@ const Live = () => {
 		}
 	}, 100);
 
-	useEventListener((eventData) => {
+	useEventListener((eventData: any) => {
 		const event = eventData.event as ReactionEvent;
 
 		setReaction((reactions) =>
@@ -160,15 +164,14 @@ const Live = () => {
 
 	return (
 		<div
+			id="canvas"
 			onPointerMove={handlePointerMove}
 			onPointerLeave={handlePointerLeave}
 			onPointerDown={handlePointerDown}
 			onPointerUp={handlePointerUp}
 			className="h-[100vh] w-full flex justify-center items-center text-center"
 		>
-			<h1 className="text-2xl text-white">
-				Ligma...what is ligma? Ligma balls!
-			</h1>
+			<canvas ref={canvasRef} />
 
 			{reaction.map((r) => (
 				<FlyingReaction
--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,13 @@ const nextConfig = {
 			},
 		],
 	},
+	webpack: (
+		config,
+		{ buildId, dev, isServer, defaultLoaders, nextRuntime, webpacl }
+	) => {
+		config.externals.push({ canvas: "commonjs canvas" });
+		return config;
+	},
 };
 
 export default nextConfig;
