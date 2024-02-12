73f9a53be9ca685372d32337a9b63b128116e60b
Samarth Shrivastava <shrivastavasamarth22@gmail.com>
2024-02-12 18:44:23 +0530

    fixed a bug in the delete functionality

--- app/page.tsx
+++ app/page.tsx
@@ -135,6 +135,10 @@ export default function Page() {
 		window.addEventListener("resize", () => {
 			handleResize({ fabricRef });
 		});
+
+		return () => {
+			canvas.dispose();
+		};
 	}, []);
 
 	useEffect(() => {
